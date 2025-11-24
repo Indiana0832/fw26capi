d260f09840b3a97c8f2e85704d04cb2297998f10
Indiana0832 <152924826+Indiana0832@users.noreply.github.com>
2025-11-24 17:39:11 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine','ejs')
+app.set('views','./views')
  // Define your routes
 
-app.get('/', (req, res) => {
-
-  res.json({ message: 'Hello from Express on Vercel!' });
-
-});
+require("../rotas/home")
 
 app.listen(port, () => {
 
